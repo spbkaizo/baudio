0ecabd20a66ad87e41b088b6415ec471e9e4baaa
Simon B <simonb@kaizo.org>
2024-03-27 13:58:23 +0000

    changes to eeprom writing speed

--- volume-control/attiny1614_volume_control/main.c
+++ volume-control/attiny1614_volume_control/main.c
@@ -1,11 +1,11 @@
-#include <avr/io.h>
-
 #ifndef F_CPU
 #define F_CPU 8000000UL
 #endif
+
+#include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#include <avr/eeprom.h> // Include the EEPROM library
+#include <avr/eeprom.h>
 
 #define LPOT_CS_bm (1 << 4)  // Use the correct bit mask for PA4
 #define RPOT_CS_bm (1 << 3)  // Use the correct bit mask for PA3
@@ -15,8 +15,10 @@
 #define VOL_DOWN_bm (1 << 6) // Use the correct bit mask for PA6
 #define LED_PIN_bm (1 << 7)  // LED pin PA7
 
-volatile uint8_t volume; // Mid-volume, marked volatile as it's accessed in ISR and main loop
+volatile uint8_t volume; // Current volume, marked volatile as it's accessed in ISR and main loop
 uint8_t EEMEM storedVolume = 127; // EEPROM variable to store volume level
+uint8_t lastStoredVolume; // To track the last stored volume value for comparison
+unsigned long writeDelayCounter = 0; // Counter to manage write delay
 
 void spi_init() {
 	PORTA.DIRSET = SCK_PIN_bm | SI_PIN_bm | LPOT_CS_bm | RPOT_CS_bm | LED_PIN_bm; // Set as output
@@ -34,7 +36,7 @@ void spi_send_byte(uint8_t byte) {
 			PORTA.OUT &= ~SI_PIN_bm;
 		}
 		PORTA.OUT |= SCK_PIN_bm;
-		_delay_us(10); // Adjust based on SPI requirements
+		_delay_us(10);
 		PORTA.OUT &= ~SCK_PIN_bm;
 	}
 }
@@ -47,17 +49,14 @@ void pot_write(uint8_t cs_bm, uint8_t value) {
 }
 
 ISR(PORTA_PORT_vect) {
-	// Immediate volume adjustment
 	if (!(PORTA.IN & VOL_UP_bm) && volume < 255) {
 		volume++;
 		pot_write(LPOT_CS_bm, volume);
 		pot_write(RPOT_CS_bm, volume);
-		eeprom_write_byte(&storedVolume, volume); // Update EEPROM
 		} else if (!(PORTA.IN & VOL_DOWN_bm) && volume > 0) {
 		volume--;
 		pot_write(LPOT_CS_bm, volume);
 		pot_write(RPOT_CS_bm, volume);
-		eeprom_write_byte(&storedVolume, volume); // Update EEPROM
 	}
 	// Debounce (simple method)
 	_delay_ms(50);
@@ -70,10 +69,21 @@ int main(void) {
 
 	// Load the volume level from EEPROM at startup
 	volume = eeprom_read_byte(&storedVolume);
+	lastStoredVolume = volume; // Initialize lastStoredVolume
 
 	while (1) {
 		// Toggle LED state
 		PORTA.OUTTGL = LED_PIN_bm;
-		_delay_ms(500); // Wait for 500 milliseconds
+		_delay_ms(1000); // Approximate 1s delay
+
+		// Increment write delay counter
+		writeDelayCounter++;
+
+		// Check if volume has changed and if sufficient delay has passed (approximating with counter)
+		if ((volume != lastStoredVolume) && (writeDelayCounter >= 120)) { // 120 cycles * 1s delay = 2 minute
+			eeprom_write_byte(&storedVolume, volume);
+			lastStoredVolume = volume;
+			writeDelayCounter = 0; // Reset counter after writing
+		}
 	}
 }
